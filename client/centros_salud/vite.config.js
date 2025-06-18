77a26801ca2734131a647bbe8a35b0ef52b2c542
Dorian Donoso Ampuero <93841138+DorianAndrea@users.noreply.github.com>
2025-06-18 19:04:21 -0400

    Corrige ruta y publica frontend en GitHub Pages

--- client/centros_salud/vite.config.js
+++ client/centros_salud/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/LocalizacionConApiUrgenciasChile/",
-  plugins: [react()],
+  base: '/LocalizacionConApiUrgenciasChile/',
 })
-
 
