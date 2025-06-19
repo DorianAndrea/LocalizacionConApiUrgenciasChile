f89877c944f2536d19b0c54243265e8a5b7c1826
Dorian Donoso Ampuero <93841138+DorianAndrea@users.noreply.github.com>
2025-06-18 21:16:17 -0400

    update init

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,11 +2,21 @@ from flask import Flask
 from flask_cors import CORS
 import os
 
-
 app = Flask(__name__)
 app.secret_key = "shhhhhh"
-frontend_url = os.getenv("FRONTEND_URL", "https://dorianandrea.github.io/LocalizacionConApiUrgenciasChile/")
 
+# URL frontend en GitHub Pages y Render
+CORS(app, resources={
+    r"/api/*": {
+        "origins": [
+            "https://frontend-localizacion.onrender.com",
+            "https://dorianandrea.github.io"
+        ],
+        "methods": ["GET", "POST", "OPTIONS"],
+        "allow_headers": ["Content-Type"]
+    }
+})
 
-CORS(app, resources={r"/api/*": {"origins": frontend_url}})
+# Variables de entorno opcionales
+frontend_url = os.getenv("FRONTEND_URL", "https://dorianandrea.github.io")
 DATABASE_URL = os.getenv("DATABASE_URL")
